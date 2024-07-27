810555639d3c98c2d33d13bfafd56da7810becd9
Antonio Oviedo <antoniooviedo131102@gmail.com>
2024-07-26 19:25:57 -0600

    semi f

--- script.js
+++ script.js
@@ -5,64 +5,78 @@ const tipo_de_imagen = "jpg";
 // -----------------------------------------
 
 const previewer = document.getElementById("previewer");
+const previewer_container = document.getElementById("prev-container");
 const viewer = document.getElementById("viewer");
 const gallery_icon = document.getElementById("gallery");
+const viewportInfo = document.getElementById("viewport").getBoundingClientRect();
 
 gallery_icon.addEventListener("click", () => {
     previewer.style.width = "8rem";
     gallery_icon.style.display = "none";
 });
 
-["touchstart", "click", "scroll"].forEach((event) => {
-    viewer.addEventListener(event, () => {
-        if (previewer.style.width == "8rem") {
-            previewer.style.width = null;
-            gallery_icon.style.display = "flex";
-        }
-    });
+const closePreviewer = () => {
+    if (previewer.style.width == "8rem") {
+        previewer.style.width = null;
+        gallery_icon.style.display = "flex";
+    }
+}
+
+["touchstart", "click"].forEach((event) => {
+    viewer.addEventListener(event, closePreviewer);
 });
+
+document.addEventListener("scroll", closePreviewer);
 
 // const addImage = (id, src, container) => {
 //     let div = document.createElement("div");
-//     div.classList.add("img-container");
-
-//     let diva = document.createElement("div");
-//     diva.classList.add("img-anchor");
-//     diva.id = nombre_de_imagen + id;
-
-//     div.appendChild(diva);
 
 //     let img = new Image();
 //     img.src = src;
-//     img.style.maxHeight = viewerInfo.height + "px";
-//     // img.id = nombre_de_imagen + id;
+//     // img.style.height = viewportInfo.height + "px";
+//     img.id = nombre_de_imagen + id;
 
 //     div.appendChild(img);
-
 //     container.appendChild(div);
 // }
 
-// const addPreviewImage = (id, src, container) => {
-//     let a = document.createElement("a");
-//     a.href = "#" +  nombre_de_imagen + id;
-//     a.classList.add("prev-card");
+const addImage = (id, src, container) => {
+    let img = new Image();
+    img.src = src;
+    img.id = nombre_de_imagen + id;
 
-//     let img = new Image();
-//     img.src = src;
-//     a.appendChild(img);
+    // img.style.alignSelf = "center";
+    // a = img.width / img.height;
+    // iw = viewportInfo.height + "px";
+    // ih =  viewportInfo.height * a + "px";
+    // img.style.maxHeight = iw;
+    // img.style.maxWidth = ih;
 
-//     let p = document.createElement("p");
-//     p.innerText = id + 1;
-//     a.appendChild(p);
+    container.appendChild(img);
+}
 
-//     container.appendChild(a);
-// }
+const addPreviewImage = (id, src, container) => {
+    let div = document.createElement("div");
+    div.classList.add("prev-card");
+
+    let a = document.createElement("a");
+    a.href = "#" +  nombre_de_imagen + id;
+    a.classList.add("prev-link");
+
+    let img = new Image();
+    img.src = src;
+    img.classList.add("prev-img");
+
+    a.appendChild(img);
+    div.appendChild(a);
+    container.appendChild(div);
+}
 
 
-// for (let i=0; i<numero_de_paginas; i++) {
-//     let src = "./paginas/" + nombre_de_imagen + i + "." + tipo_de_imagen;
+for (let i=0; i<numero_de_paginas; i++) {
+    let src = "./paginas/" + nombre_de_imagen + i + "." + tipo_de_imagen;
 
-//     addImage(i, src, viewer);
+    addImage(i, src, viewer);
 
-//     addPreviewImage(i, src, previewer);
-// }
+    addPreviewImage(i, src, previewer_container);
+}
